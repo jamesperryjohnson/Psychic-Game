bc2770323ded0c889c46848be97805972fac172b
James Perry Johnson <Perry.Johnson@vml.com>
2018-02-04 05:26:56 -0600

    Refactor to update progress bar per guess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,8 @@
 
 var wins = 0;
 var losses = 0;
-var guessLeft = 9;
+var maxGuess = 9;
+var guessLeft = maxGuess;
 var selectedLetters = new Array();
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
@@ -38,7 +39,7 @@ function lostRound() {
 }
 
 function reset() {
-    guessLeft = 9;
+    guessLeft = maxGuess;
     selectedLetters = [];
     clearGuesses();
     updateDisplay();
@@ -50,24 +51,26 @@ function clearGuesses() {
 }
 
 function updateProgressBar() {
+    var guessPercentLeft = ((guessLeft / maxGuess) * 100);
+
     if (guessLeft >= 7) {
         $("#guess-progress").addClass("bg-success");
-        $("#guess-progress").attr("style", "width: 100%");
-        $("#guess-progress").attr("aria-valuenow", "9")
+        $("#guess-progress").attr("style", "width: " + guessPercentLeft + "%");
+        $("#guess-progress").attr("aria-valuenow", guessLeft)
         $("#guess-progress").removeClass("bg-warning");
         $("#guess-progress").removeClass("bg-danger");
     } else if (guessLeft >= 4) {
         $("#guess-progress").removeClass("bg-success");
         $("#guess-progress").addClass("bg-warning");
-        $("#guess-progress").attr("style", "width: 66%");
-        $("#guess-progress").attr("aria-valuenow", "6")
+        $("#guess-progress").attr("style", "width: " + guessPercentLeft + "%");
+        $("#guess-progress").attr("aria-valuenow", guessLeft)
         $("#guess-progress").removeClass("bg-danger");
     } else {
         $("#guess-progress").removeClass("bg-success");
         $("#guess-progress").removeClass("bg-warning");
         $("#guess-progress").addClass("bg-danger");
-        $("#guess-progress").attr("style", "width: 33%");
-        $("#guess-progress").attr("aria-valuenow", "3")
+        $("#guess-progress").attr("style", "width: " + guessPercentLeft + "%");
+        $("#guess-progress").attr("aria-valuenow", guessLeft)
     }
 }
 
